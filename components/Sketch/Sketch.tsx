d06324140404934794dba972e8d319cca9db2282
airtoxin <airtoxin@icloud.com>
2021-12-26 15:02:30 +0900

    Add sketch component

---
+++ components/Sketch/Sketch.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useRef, VoidFunctionComponent } from "react";
+import P5 from "p5";
+
+export type Props = {
+  setup: (p5: P5, parent: HTMLDivElement) => void;
+  draw: (p5: P5) => void;
+};
+const Sketch: VoidFunctionComponent<Props> = ({setup, draw}) => {
+  const canvasParentRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const sketch = new P5((p: P5) => {
+      p.setup = () => {
+        if (canvasParentRef.current != null) {
+          setup(p, canvasParentRef.current);
+        }
+      };
+      p.draw = () => draw(p);
+    });
+    return () => sketch.remove();
+  }, [draw, setup]);
+
+  return (
+    <div
+      ref={canvasParentRef}
+    />
+  );
+}
+
+export default Sketch;

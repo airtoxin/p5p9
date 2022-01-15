19da87797c4004afd93cf5a286363624053425a5
airtoxin <airtoxin@icloud.com>
2022-01-16 00:06:56 +0900

    Add template

---
+++ pages/sketches/000_template.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from "next";
+import { Sketch } from "../../components/Sketch";
+import P5 from "p5";
+import { P5Utils } from "../../utils";
+import { HandDrawLine } from "../../HandDraw";
+
+const SketchPage: NextPage = () => {
+  const setup = (p5: P5, utils: P5Utils) => {
+    p5.background(255);
+    p5.randomSeed(1);
+    p5.noiseSeed(1);
+    p5.smooth();
+    p5.stroke(0);
+    p5.noFill();
+    p5.noLoop();
+  };
+
+  const draw = (p5: P5, utils: P5Utils) => {};
+  return <Sketch setup={setup} draw={draw} />;
+};
+
+export default SketchPage;
